96f988c48f46addbcd3df3fe165b094e0d03b4e5
Sandi Junker <sandi.junker@outlook.com>
2024-06-13 14:44:54 -0500

    Complete chamber join page

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -4,5 +4,8 @@ const date = new Date();
 document.getElementById("lastModified").innerHTML = `Last Modified: ${date.toLocaleDateString("en-US")}`;
 document.querySelector("span").innerHTML = date.getFullYear();
 
+//Time and date form submitted
+document.getElementById("dateTime").value = date;
+
 
 
