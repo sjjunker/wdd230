a9bd52028dc2d733245d341df8c5c552c800f4d1
Sandi Junker <sandi.junker@outlook.com>
2024-06-18 15:09:06 -0500

    Finish json link list

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,32 @@
 const baseURL = "https://sjjunker.github.io/wdd230/";
-const linksURL = "https://sjjunker.github.io/wdd230/data/links.json"
+const linksURL = "https://sjjunker.github.io/wdd230/data/links.json";
+const unorderedList = document.querySelector("#learning-activities");
 
 async function getLinks() {
     const promise = await fetch(linksURL);
     const data = await promise.json();
     console.log(data);
+
+    displayLinks(data);
+}
+
+function displayLinks(weeks) {
+    weeks.forEach((week) => {
+        //Create Elements
+        let listItem = document.createElement("li");
+        listItem.innerHTML = `Week ${week.lesson}:`;
+
+        //Go through each link in the lesson
+        week.list.forEach((link) => {
+            let linkItem = document.createElement("a");
+            linkItem.setAttribute("href", link.url);
+            linkItem.innerHTML = ` ${link.title}`;
+            listItem.appendChild(linkItem);
+        });
+
+        //Append to the unordered list
+        unorderedList.appendChild(listItem);
+    });
 }
 
 getLinks();
