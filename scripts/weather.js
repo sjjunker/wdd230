673f12bc2ac4634ac5b2338254bcc3621f9a951f
Sandi Junker <sandi.junker@outlook.com>
2024-06-26 06:39:16 -0500

    Add weather api to index.html

---
+++ scripts/weather.js
@@ -0,0 +1,36 @@
+//Get HTML Elements
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+//API url
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=b119c36cdfc204d7f88190d1fd8b5ff4";
+
+//Fetch
+async function apiFetch() {
+    try {
+        let response = await fetch(url);
+        if (response.ok) {
+            let data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//Call fetch function
+apiFetch();
+
+//Display the results
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&degF`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    captionDesc.textContent = `${desc}`;
+}
