61ec1fe7506349b5b0737fd54de916eebf794f1e
Sandi Junker <sandi.junker@outlook.com>
2024-07-11 09:23:56 -0500

    Finish homepage. Start reservations page.

---
+++ scoots/scripts/banner.js
@@ -0,0 +1,5 @@
+const banner = document.querySelector("#banner");
+
+banner.addEventListener("click", () => {
+    banner.setAttribute("style", "opacity: 0");
+});
--- scoots/scripts/weather.js
+++ scoots/scripts/weather.js
@@ -4,7 +4,8 @@ const currentHum = document.querySelector("#humidity");
 const weatherIcon = document.querySelector("#weather-icon");
 const weatherDesc = document.querySelector("#weather-description");
 const tomorrowsHigh = document.querySelector("#tomorrows-high");
-
+const weatherTitle = document.querySelector("#weather-title");
+const highTemp = document.querySelector("#banner");
 
 //API url
 const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=20.4&lon=-86.9&units=imperial&appid=b119c36cdfc204d7f88190d1fd8b5ff4";
@@ -30,12 +31,14 @@ apiFetch();
 
 //Display the results
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.list[0].main.temp}&degF`;
-    currentHum.innerHTML = `${data.list[0].main.humidity}%`;
+    weatherTitle.innerHTML = data.list[0].weather[0].main;
+    currentTemp.innerHTML = `Current temp: ${(data.list[0].main.temp).toFixed(0)}&degF`;
+    currentHum.innerHTML = `Humidity: ${data.list[0].main.humidity}%`;
     const iconsrc = `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`;
     let desc = data.list[0].weather[0].description;
     weatherIcon.setAttribute("src", iconsrc);
     weatherIcon.setAttribute("alt", desc);
     weatherDesc.innerHTML = `${desc}`;
     tomorrowsHigh.innerHTML = `High: ${data.list[1].main.temp_max}&degF`;
+    highTemp.innerHTML = `High: ${(data.list[0].main.temp_max).toFixed(0)}&degF`;
 }
