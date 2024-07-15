13ee9953d7247260efbcf2dbc0dd9e2cf63aca4c
Sandi Junker <sandi.junker@outlook.com>
2024-07-15 06:44:27 -0500

    Finish Rentals Page

---
+++ scoots/scripts/rentals-rentals.js
@@ -0,0 +1,44 @@
+const rentalTable = document.getElementById("rental-vehicles");
+const url = "https://sjjunker.github.io/wdd230/scoots/data/rentals.json";
+
+
+async function getRentals(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayRentals(data.rentals);
+}
+
+function displayRentals(rentals) {
+
+    rentals.forEach(rental => {
+        //Create the card and its elements
+        let tableRow = document.createElement("tr");
+        let name = document.createElement("th");
+        let maxPeople = document.createElement("td");
+        let halfReserv = document.createElement("td");
+        let fullReserv = document.createElement("td");
+        let halfWalk = document.createElement("td");
+        let fullWalk = document.createElement("td");
+
+        //Set the attributes
+        name.innerHTML = rental.name;
+        maxPeople.innerHTML = rental.max-people;
+        halfReserv.innerHTML = rental.half-day-reservation;
+        fullReserv.innerHTML = rental.full-day-reservation;
+        halfWalk.innerHTML = rental.half-day-walk;
+        fullWalk.innerHTML = rental.full-day-walk;
+
+        //Append the elements to the card
+        tableRow.appendChild(name);
+        tableRow.appendChild(maxPeople);
+        tableRow.appendChild(halfReserv);
+        tableRow.appendChild(fullReserv);
+        tableRow.appendChild(halfWalk);
+        tableRow.appendChild(fullWalk);
+
+        //Add the card to the DOM
+        rentalTable.appendChild(tableRow);
+    });
+}
+
+getRentals(url);
